6f15df398ad4bd440be6ede3eefbb445a2b80e33
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-04-21 02:07:10 +0300

    update (PersonService): improved by redis

--- src/main/java/ru/stepagin/atiomid/service/PersonService.java
+++ src/main/java/ru/stepagin/atiomid/service/PersonService.java
@@ -1,8 +1,13 @@
 package ru.stepagin.atiomid.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import ru.stepagin.atiomid.DTO.PersonDTO;
 import ru.stepagin.atiomid.entity.PersonEntity;
 import ru.stepagin.atiomid.repository.PersonRepo;
@@ -11,9 +16,35 @@ import ru.stepagin.atiomid.repository.PersonRepo;
 public class PersonService {
     @Autowired
     private PersonRepo personRepo;
+    @Autowired
+    private ObjectMapper jacksonObjectMapper;
+    @Value("${app.redis.host}")
+    private String redisHost;
+    @Value("${app.redis.port}")
+    private int redisPort;
+    private final JedisPool jedisPool = new JedisPool(redisHost, redisPort);
 
-    public PersonEntity getPerson(String author) {
-        return personRepo.findById(author).orElse(null);
+
+    public PersonEntity getPerson(String name) {
+        return getCachedPerson(name);
+    }
+
+    private PersonEntity getCachedPerson(String name) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            String key = "person:" + name;
+            String raw = jedis.get(key);
+            if (raw != null) {
+                return jacksonObjectMapper.readValue(raw, PersonEntity.class);
+            }
+            PersonEntity person = personRepo.findById(name).orElse(null);
+            if (person == null) {
+                return null;
+            }
+            jedis.setex(key, 10, jacksonObjectMapper.writeValueAsString(person));
+            return person;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public PersonDTO registerPerson(@Nonnull String name, @Nonnull String password) {
